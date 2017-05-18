ad088cf5a823027e2fe402f357cd49047aeee2c9
HelderSepu <HelderSepu@gmail.com>
2017-05-18 12:01:37 -0400

    Remove initializations from the Program.cs

--- coldcuts/Program.cs
+++ coldcuts/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using Un4seen.Bass;
 
 namespace ColdCutsNS{
 
@@ -13,13 +12,7 @@ namespace ColdCutsNS{
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            //initialize bass.net. this seemed like the best place
-           
-            if (Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero)) {
-
-                Application.Run(new MainForm());
-            }
+            Application.Run(new MainForm());
         }
     }
 }
