f3144ca94cfd1011fbc9d36e5df6aa3b9b196fa2
HelderSepu <heldersepu@gmail.com>
2017-06-14 08:30:46 -0400

    Add second marker

--- coldcuts/ImageForm.cs
+++ coldcuts/ImageForm.cs
@@ -7,7 +7,8 @@ namespace ColdCutsNS
 {
     public partial class ImageForm : Form
     {
-        Pen pen = new Pen(Color.LimeGreen, 1);
+        Pen penGreen = new Pen(Color.LimeGreen, 1);
+        Pen penRed = new Pen(Color.Red, 1);
         bool busy = false;
 
         public ImageForm()
@@ -22,18 +23,30 @@ namespace ColdCutsNS
             busy = true;
             double max = 0;
             int count = sound.Count;
-            for (int i = 0; i < count; i++)
-                if (sound[i] > max) max = sound[i];
-            var b = new Bitmap(count, pictureBox.Height);
-            using (var g = Graphics.FromImage(b))
+            if (count < 90000)
             {
                 for (int i = 0; i < count; i++)
+                    if (sound[i] > max) max = sound[i];
+                var b = new Bitmap(count, pictureBox.Height);
+                using (var g = Graphics.FromImage(b))
                 {
-                    g.DrawLine(pen, i, 0, i, (int)Math.Round((sound[i]/max) * pictureBox.Height));
+                    for (int i = 1; i < count; i++)
+                    {
+                        g.DrawLine(penGreen, i, 0, i, (int)Math.Round((sound[i] / max) * pictureBox.Height));
+                        if (i % 100 == 0)
+                        {
+                            g.DrawLine(penRed, i, 0, i, 10);
+                            if (i % 500 == 0)
+                            {
+                                g.DrawLine(penRed, i - 1, 0, i - 1, 6);
+                                g.DrawLine(penRed, i - 2, 0, i - 2, 2);
+                            }
+                        }
+                    }
                 }
+                pictureBox.Image = b;
+                pictureBox.Width = count;
             }
-            pictureBox.Image = b;
-            pictureBox.Width = count;
             busy = false;
         }
     }
