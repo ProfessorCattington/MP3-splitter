867f1d6092d3e30292afd28ee4b3f10493f8d153
U-Corsair\Mike <mister.catsby@gmail.com>
2017-07-21 12:00:05 -0700

    -a doesn't want to add the soundPicture.cs file

---
+++ coldcuts/SoundPicture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ColdCutsNS
+{
+    class SoundPicture : PictureBox
+    {
+        public SoundPicture(Bitmap b, int width, int height)
+        {
+            Image = b;
+            Width = width;
+            Height = height;
+            Location = new Point(0, 0);
+            Margin = new Padding(0);
+        }
+    }
+}
